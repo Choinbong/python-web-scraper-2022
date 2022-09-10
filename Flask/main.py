cb438ff0a5f2f3b32ee7dbaad4250536483009f5
Choinbong <101409368+Choinbong@users.noreply.github.com>
2022-09-10 22:34:48 +0900

    Update main.py

--- Flask/main.py
+++ Flask/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
 
-app = Flask("JobScrapper")
+app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", name="JH")  # {{name}} in a html file
 
 
 @app.route("/search")
@@ -14,4 +14,5 @@ def hello():
     return render_template("search.html")
 
 
-app.run("127.0.0.1")
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
