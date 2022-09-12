ebddd70dd45e546c4e1a6d7536a8fa100a4918a4
Choinbong <101409368+Choinbong@users.noreply.github.com>
2022-09-12 15:16:48 +0900

    study

--- Flask/main.py
+++ Flask/main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.indeed import extract_indeed_jobs
+from extractors.indeed import extract_wwr_jobs
+from file import save_to_file
 
 
 app = Flask(__name__)
+
+db = {}
 
 
 @app.route("/")
@@ -11,7 +16,28 @@ def home():
 
 @app.route("/search")
 def hello():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 if __name__ == "__main__":
